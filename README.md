a0d36e9f61dee86df62afc718e2cab8295a65869
Pawel Papierkowski <pawel.papierkowski@gmail.com>
2026-02-08 17:04:12 +0100

    Initial commit (almost blank Vue project).
    Add custom icon, change title and default text from App.vue.

---
+++ README.md
@@ -0,0 +1,3 @@
+# tic-tac-toe
+
+Very simple project showing off the very basics of Vue frontend project.
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <p>
+    TODO: actual tic-tac-toe.
+  </p>
+</template>
+
+<style scoped>
+</style>
